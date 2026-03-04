21a705c754bdaf2e7fccda8f7dae1651db99746e
rakesh ks <rakeshksofficelaptop_swivl_tech@MacBookAir.lan>
2026-03-04 13:14:14 +0530

    Initial TestNG framework commit

---
+++ src/test/java/com/simple/pages/LoginPage.java
@@ -0,0 +1,15 @@
+package com.simple.pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    WebDriver driver;
+    public LoginPage(WebDriver driver) { this.driver = driver; }
+
+    By searchBox = By.name("q");
+
+    public void search(String text) {
+        driver.findElement(searchBox).sendKeys(text);
+        driver.findElement(searchBox).submit();
+    }
+}
---
+++ src/test/java/com/simple/tests/SearchTest.java
@@ -0,0 +1,32 @@
+package com.simple.tests;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.*;
+import com.simple.pages.LoginPage;
+
+public class SearchTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // Vital for Jenkins
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void testGoogleSearch() {
+        driver.get("https://www.google.com");
+        LoginPage lp = new LoginPage(driver);
+        lp.search("Selenium Jenkins CI/CD");
+        System.out.println("Search executed successfully");
+        System.out.println("Page title: " + driver.getTitle());
+    }
+
+    @AfterMethod
+    public void teardown() 
+    { 
+        driver.quit(); 
+    }
+}
