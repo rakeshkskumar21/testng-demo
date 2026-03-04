d08ceb42fb8cc504d5a6c164f51298ee92ec64b7
rakesh ks <rakeshksofficelaptop_swivl_tech@MacBookAir.lan>
2026-03-04 19:35:37 +0530

    2nd change

--- src/test/java/com/simple/tests/testSwivlLogin.java
+++ src/test/java/com/simple/tests/testSwivlLogin.java
@@ -7,9 +7,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
-import com.simple.pages.LoginPage;
 import io.qameta.allure.Attachment;
-public class SearchTest {
+
+public class testSwivlLogin {
     WebDriver driver;
 
     @BeforeMethod
@@ -20,19 +20,17 @@ public class SearchTest {
     }
 
     @Test
-    public void testswivllogin() {
-        driver.navigate().to("https://uat.swivl.tech/");
-        //------open the website---------
+    public void testswivllogin() throws InterruptedException {
+        ChromeDriver driver = new ChromeDriver();
+    //------open the website---------
     driver.navigate().to("https://uat.swivl.tech/"); // UAT/stage/dev build
     driver.manage().window().maximize();
     //Thread.sleep(2000);
-     WebElement username = driver.findElement(By.xpath("//*[@id=\"username-label\"]"));
-     username.sendKeys("uatfeb2026-1@yopmail.com");
-     WebElement password = driver.findElement(By.xpath("//*[@id=\"password\"]"));
-     password.sendKeys("Password123!");
+     driver.findElement(By.xpath("/html/body/div/main/section/div/div[2]/div/form/div[1]/div/div[1]/div[1]/input")).sendKeys("uatfeb2026-1@yopmail.com");
+     driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("Password123!");
      //Thread.sleep(1000);
      driver.findElement(By.xpath("/html/body/div/main/section/div/div[2]/div/form/div[2]/button")).click();
-        System.out.println("Page title: " + driver.getTitle());
+    System.out.println("Page title: " + driver.getTitle());
     }
 
     @Attachment(value = "Page screenshot", type = "image/png")
