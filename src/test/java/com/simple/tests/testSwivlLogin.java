c9d797fb202b6a00799946ed7d843e3f79f00b10
rakesh ks <rakeshksofficelaptop_swivl_tech@MacBookAir.lan>
2026-03-04 20:20:46 +0530

    fd

--- src/test/java/com/simple/tests/testSwivlLogin.java
+++ src/test/java/com/simple/tests/testSwivlLogin.java
@@ -25,12 +25,12 @@ public class testSwivlLogin {
     //------open the website---------
     driver.navigate().to("https://uat.swivl.tech/"); // UAT/stage/dev build
     driver.manage().window().maximize();
-    //Thread.sleep(2000);
+    Thread.sleep(2000);
      driver.findElement(By.xpath("/html/body/div/main/section/div/div[2]/div/form/div[1]/div/div[1]/div[1]/input")).sendKeys("uatfeb2026-1@yopmail.com");
      driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("Password123!");
-     //Thread.sleep(1000);
+     Thread.sleep(2000);
      driver.findElement(By.xpath("/html/body/div/main/section/div/div[2]/div/form/div[2]/button")).click();
-    System.out.println("Page title: " + driver.getTitle());
+    System.out.println("Page title: " );
     }
 
     @Attachment(value = "Page screenshot", type = "image/png")
